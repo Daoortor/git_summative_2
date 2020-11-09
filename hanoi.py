06fcd81826a04a221dddbd5a262effac9b61a35e
David Dashevskiy <david.dshvsk@gmail.com>
2020-11-09 10:52:46 +0300

    add recursive solution

--- hanoi.py
+++ hanoi.py
@@ -1,1 +1,11 @@
-
+def hanoi(n, start=1, end=3):
+	if n == 1:
+		return [(start, end)]
+	else:
+		if set(start, end) == set(1, 2):
+			free = 3
+		elif set(start, end) == set(1, 3):
+			free = 2
+		else:
+			free = 1
+		return hanoi(n - 1, start, free) + [(start, end)] + hanoi(n - 1, free, end)
